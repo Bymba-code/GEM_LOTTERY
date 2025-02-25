842b5eeb7ffd692d7b6862d313c1d81a63b55221
Bymba-code <bymba6607@gmail.com>
2025-02-25 16:05:56 +0800

    Update index.js

--- Controllers/2. Lottery/1. GET_ALL/index.js
+++ Controllers/2. Lottery/1. GET_ALL/index.js
@@ -3,7 +3,13 @@ const prisma = require("../../../Middlewares/prisma")
 const GET_ALL_LOTTERY = async (req, res) => {
     try 
     {
-        const lottery = await prisma.lottery.findMany({})
+        const lottery = await prisma.lottery.findMany({
+            include:{
+                lottery_description_lottery_description_lotteryTolottery:true,
+                lottery_users_lottery_users_lotteryTolottery:true,
+                lottery_winners_lottery_winners_lotteryTolottery:true
+            }
+        })
 
         return res.status(200).json({
             success:true,
