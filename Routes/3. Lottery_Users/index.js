0c1e10e13e81b3a327bc5d40c1f5bfbdda3c40cc
Bymba-code <bymba6607@gmail.com>
2025-02-25 16:32:36 +0800

    Update index.js

--- Routes/3. Lottery_Users/index.js
+++ Routes/3. Lottery_Users/index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const INSERT_LOTTERY_USERS = require("../../Controllers/3. UserLotteryController/3. POST")
+const GET_ALL_LOTTERY_USERS = require("../../Controllers/3. UserLotteryController/1. GET_ALL")
 
 const router = express.Router()
 
-router.route("/lottery/user").post(INSERT_LOTTERY_USERS)
+router.route("/lottery/user")
+.post(INSERT_LOTTERY_USERS)
+
+router.route("/lottery/users/:id").get(GET_ALL_LOTTERY_USERS)
 
 module.exports = router
