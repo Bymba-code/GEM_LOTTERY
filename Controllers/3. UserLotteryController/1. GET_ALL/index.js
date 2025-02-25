a0cd54e24d240b2cb9021afa47d6880b3774efca
Bymba-code <bymba6607@gmail.com>
2025-02-25 16:32:26 +0800

    Update index.js

--- Controllers/3. UserLotteryController/1. GET_ALL/index.js
+++ Controllers/3. UserLotteryController/1. GET_ALL/index.js
@@ -3,20 +3,26 @@ const prisma = require("../../../Middlewares/prisma")
 const GET_ALL_LOTTERY_USERS = async (req, res) => {
     try 
     {
-        const lottery = await prisma.lottery.findMany({})
-
+        const {id} = req.params
+        const lottries = await prisma.lottery_users.findMany({
+            where: {
+                lottery: parseInt(id)
+            }
+        })
+        
         return res.status(200).json({
             success:true,
-            data:lottery,
+            data:lottries,
             message: "Амжилттай"
         })
     }
     catch(err)
     {
+        console.log(err)
         return res.status(500).json({
             success:false,
             data:[],
-            message: "Серверийн алдаа гарлаа."
+            message: "Серверийн алдаа гарлаа." + err
         })
     }
 }
