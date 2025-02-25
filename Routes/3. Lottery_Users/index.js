6aeb098d41580580ee32ee744754038604b47ea1
Bymba-code <bymba6607@gmail.com>
2025-02-25 17:46:08 +0800

    Update index.js

--- Routes/3. Lottery_Users/index.js
+++ Routes/3. Lottery_Users/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const INSERT_LOTTERY_USERS = require("../../Controllers/3. UserLotteryController/3. POST")
 const GET_ALL_LOTTERY_USERS = require("../../Controllers/3. UserLotteryController/1. GET_ALL")
 const UPDATE_USER_LOTTERY = require("../../Controllers/3. UserLotteryController/4. UPDATE")
+const GET_RANDOM_USER_FROM_LOTTERY = require("../../Controllers/3. UserLotteryController/5. WINNER")
 
 const router = express.Router()
 
@@ -9,5 +10,8 @@ router.route("/lottery/user")
 .post(INSERT_LOTTERY_USERS)
 
 router.route("/lottery/users/:id").get(GET_ALL_LOTTERY_USERS).put(UPDATE_USER_LOTTERY)
+
+router.route("/lottery/winner/:id").get(GET_RANDOM_USER_FROM_LOTTERY)
+
 
 module.exports = router
